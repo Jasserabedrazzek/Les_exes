4d5a09d8de7f1d4ed6979793022f9eb7408ed043
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-12 02:42:53 +0100

    Update 3eme.py

--- 3eme.py
+++ 3eme.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 import base64
 import sqlite3
+import time
 
 st.set_page_config(
     page_title="3eme secondaire",
@@ -13,7 +14,7 @@ st.set_page_config(
 
 st.title("3eme secondaire")
 st.write("---")
-st.header("Les séries")
+st.header("Upload Files PDF or Doc ")
 
 upload_folder = "uploads"
 os.makedirs(upload_folder, exist_ok=True)
@@ -27,13 +28,27 @@ c.execute('''CREATE TABLE IF NOT EXISTS files
              (name TEXT, data BLOB)''')
 conn.commit()
 
-# Function to save uploaded file to the database
-def save_file_to_db(file):
-    with open(file, 'rb') as f:
-        file_data = f.read()
-    file_name = Path(file).name
-    c.execute("INSERT INTO files (name, data) VALUES (?, ?)", (file_name, sqlite3.Binary(file_data)))
-    conn.commit()
+# Function to save uploaded file to the database with retry mechanism
+def save_file_to_db_with_retry(file):
+    retries = 0
+    while True:
+        try:
+            with open(file, 'rb') as f:
+                file_data = f.read()
+            file_name = Path(file).name
+            c.execute("INSERT INTO files (name, data) VALUES (?, ?)", (file_name, sqlite3.Binary(file_data)))
+            conn.commit()
+            st.success("File uploaded successfully.")
+            break
+        except sqlite3.OperationalError as e:
+            if 'database is locked' in str(e).lower() and retries < 5:
+                retries += 1
+                delay = 2 ** retries  # exponential backoff
+                st.warning(f"Database is locked. Retrying in {delay} seconds...")
+                time.sleep(delay)
+            else:
+                st.error("Failed to upload file. Please try again later.")
+                break
 
 # Function to retrieve file from the database
 def retrieve_file_from_db(file_name):
@@ -42,19 +57,18 @@ def retrieve_file_from_db(file_name):
     return file_data
 
 # Create an upload button
-file = st.file_uploader("Upload files (PDF, DOC, images)", type=["pdf", "doc", "jpg", "jpeg", "png"])
+file = st.file_uploader("Upload files (PDF, DOC)", type=["pdf", "doc"])
 
 if file is not None:
     file_path = os.path.join(upload_folder, file.name)
     with open(file_path, "wb") as f:
         f.write(file.getbuffer())
-    save_file_to_db(file_path)
-    st.success("File uploaded successfully.")
+    save_file_to_db_with_retry(file_path)
 
 # Display uploaded files
 uploaded_files = [f for f in os.listdir(upload_folder) if os.path.isfile(os.path.join(upload_folder, f))]
 if len(uploaded_files) > 0:
-    st.header("Uploaded Files")
+    st.header("Les séries")
     for uploaded_file in uploaded_files:
         file_data = retrieve_file_from_db(uploaded_file)
         st.write(uploaded_file)
