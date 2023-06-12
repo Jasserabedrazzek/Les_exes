432cf44728f9c39afa2ad06c6d74a95298b06bf1
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-12 01:13:24 +0100

    Update 1er.py

--- 1er.py
+++ 1er.py
@@ -17,6 +17,26 @@ with col2 :
   st.header("Les series")
   
   
+
+st.write("---")
+uploads = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+if uploads:
+        for file in uploads:
+            df = pd.read_csv(file)
+            # Convert DataFrame to JSON
+            json_data = df.to_json(orient='records')
+            # Save JSON data to a file
+            filename = f"{file.name.split('.')[0]}.json"
+            with open(filename, 'w') as f:
+                f.write(json_data)
+            st.write(f"File saved as {filename}")
   
-  st.write("---")
-  uploads = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+    # Display the uploaded files
+    uploaded_files = [file for file in os.listdir() if file.endswith('.json')]
+    for file in uploaded_files:
+        with open(file, 'r') as f:
+            json_data = f.read()
+        st.write("### File:", file)
+        # Parse JSON data
+        df = pd.read_json(json_data)
+        st.dataframe(df)
