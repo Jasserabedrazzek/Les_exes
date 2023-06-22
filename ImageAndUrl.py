bf49f7625f29c18c7c8096da8dcc530c38cac300
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-22 23:56:56 +0100

    Update ImageAndUrl.py

--- ImageAndUrl.py
+++ ImageAndUrl.py
@@ -23,15 +23,12 @@ st.set_page_config(
 # Display title
 st.title('Bac 2024 doc')
 st.header("Images")
-
 # Create a button to upload an image
 uploaded_file = st.file_uploader("Upload an image")
 st.header("Liens")
-
 # Create an input field to enter a URL (optional)
 url = st.text_input("Enter a URL (optional)")
 st.header("Images Uploaded")
-
 # Save the uploaded image or URL to the database
 if uploaded_file or url:
     if uploaded_file:
@@ -41,18 +38,16 @@ if uploaded_file or url:
         cursor.execute("INSERT INTO images (url) VALUES (?)", (url,))
     conn.commit()
 
-# Retrieve the stored images or URLs from the database
+# Retrieve the stored image or URL from the database
 cursor.execute("SELECT * FROM images")
-results = cursor.fetchall()
-
-# Display the images
-if results:
-    for image_id, image_blob, image_url in results:
-        if image_blob:
-            st.image(image_blob, caption="Uploaded Image")
-            st.download_button("Download Image", data=image_blob, file_name="image.png")
-        elif image_url:
-            st.image(image_url, caption="URL Image")
+result = cursor.fetchone()
+if result:
+    image_id, image_blob, image_url = result
+    if image_blob:
+        st.image(image_blob, caption="Uploaded Image")
+        st.download_button("Download Image", data=image_blob, file_name="image.png")
+    elif image_url:
+        st.image(image_url, caption="URL Image")
 
 # Close the database connection
 cursor.close()
