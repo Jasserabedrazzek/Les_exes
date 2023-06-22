197ca4748a9e9cbfc9efdd7c2463eab8d0cf1602
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-23 00:22:44 +0100

    Update 4eme.py

--- 4eme.py
+++ 4eme.py
@@ -1,61 +1,64 @@
 import streamlit as st
-import os
-import base64
 import sqlite3
 
-# Create a connection to the SQLite database
-conn = sqlite3.connect('uploads.db')
+# Connect to the SQLite database
+conn = sqlite3.connect('uploaded_files.db')
 c = conn.cursor()
 
-# Create the 'uploads' table if it doesn't exist
-c.execute('''CREATE TABLE IF NOT EXISTS uploads
-             (file_name TEXT, file_data BLOB)''')
-
-# Function to save file to the database
-def save_file_to_db(file):
-    file_data = file.read()
-    file_name = file.name
-    c.execute("INSERT INTO uploads (file_name, file_data) VALUES (?, ?)", (file_name, file_data))
-    conn.commit()
-
-# Function to retrieve all uploaded files from the database
-def get_uploaded_files():
-    c.execute("SELECT file_name FROM uploads")
-    files = c.fetchall()
-    return files
-
-# Function to download a file from the database
-def download_file(file_name):
-    c.execute("SELECT file_data FROM uploads WHERE file_name=?", (file_name,))
-    file_data = c.fetchone()[0]
-    b64_data = base64.b64encode(file_data).decode()
-    return f'<a href="data:application/octet-stream;base64,{b64_data}" download="{file_name}">Download</a>'
+# Create the 'files' table if it doesn't exist
+c.execute('''CREATE TABLE IF NOT EXISTS files
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              file BLOB,
+              file_type TEXT)''')
+conn.commit()
 
 # Set Streamlit page configuration
 st.set_page_config(
     page_title="4eme secondaire Pdf",
+    page_icon="",
     layout="centered",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="expanded",
 )
 
-# Main title
+# Function to insert file into the database
+def insert_file(file, file_type):
+    c.execute("INSERT INTO files (file, file_type) VALUES (?, ?)", (file, file_type))
+    conn.commit()
+
+# Function to retrieve all uploaded files from the database
+def get_uploaded_files():
+    c.execute("SELECT * FROM files")
+    return c.fetchall()
+
+# Streamlit web application
 st.title('Bac 2024 doc')
 
-# File upload section
-file = st.file_uploader("Upload PDF, DOC, or Image", type=["pdf", "doc", "jpeg", "jpg", "png"])
-
-# Upload the file and save it to the database
-if file is not None:
-    save_file_to_db(file)
+# Upload PDF or DOC button
+uploaded_file = st.file_uploader("Upload PDF or DOC file", type=["pdf", "docx"])
+if uploaded_file is not None:
+    file_type = uploaded_file.type.split('/')[1]
+    insert_file(uploaded_file.read(), file_type)
     st.success('File uploaded successfully!')
+
+# Upload image button
+uploaded_image = st.file_uploader("Upload Image", type=["png", "jpg", "jpeg"])
+if uploaded_image is not None:
+    file_type = uploaded_image.type.split('/')[1]
+    insert_file(uploaded_image.read(), file_type)
+    st.success('Image uploaded successfully!')
 
 # Show uploaded files
 uploaded_files = get_uploaded_files()
-if uploaded_files:
-    st.subheader('Uploaded Files')
-    for file in uploaded_files:
-        file_name = file[0]
-        st.write(file_name)
-        st.markdown(download_file(file_name), unsafe_allow_html=True)
-else:
-    st.subheader('No files uploaded yet.')
+for file in uploaded_files:
+    file_id, _, file_type = file
+    if file_type in ["pdf", "docx"]:
+        st.write(f"File ID: {file_id}")
+        download_button = st.button("Download", key=f"download_{file_id}")
+        if download_button:
+            st.download_button(label="Download", data=file[1], file_name=f"file.{file_type}")
+    elif file_type in ["png", "jpg", "jpeg"]:
+        st.write(f"File ID: {file_id}")
+        st.image(file[1], use_column_width=True)
+
+# Close the database connection
+conn.close()
