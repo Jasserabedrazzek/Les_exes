bc0ee572b9961612da0e86f60e38dde11b7716c6
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-23 00:05:15 +0100

    Update ImageAndUrl.py

--- ImageAndUrl.py
+++ ImageAndUrl.py
@@ -1,18 +1,17 @@
 import streamlit as st
 import sqlite3
-from PIL import Image
-import requests
-from io import BytesIO
 
 # Create a connection to the SQLite database
 conn = sqlite3.connect('image_urls.db')
 cursor = conn.cursor()
 
 # Create a table to store image URLs
-cursor.execute('''CREATE TABLE IF NOT EXISTS images
-                  (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                  image BLOB,
-                  url TEXT)''')
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS images (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        url TEXT NOT NULL
+    )
+''')
 conn.commit()
 
 # Set Streamlit page configuration
@@ -23,43 +22,43 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-# Display title
+# Display the app title
 st.title('Bac 2024 doc')
 
-# Create a button to upload an image
-uploaded_file = st.file_uploader("Upload an image")
+# Create a file uploader for images
+uploaded_files = st.file_uploader("Upload Images", accept_multiple_files=True, type=['png', 'jpg', 'jpeg'])
 
-# Create an input field to enter a URL (optional)
-url = st.text_input("Enter a URL (optional)")
+# Loop over uploaded images and save them to the database
+for uploaded_file in uploaded_files:
+    if uploaded_file is not None:
+        file_bytes = uploaded_file.read()
+        cursor.execute('INSERT INTO images (url) VALUES (?)', (file_bytes,))
+        conn.commit()
 
-# Save the uploaded image or URL to the database
-if uploaded_file or url:
-    if uploaded_file:
-        image = uploaded_file.read()
-        cursor.execute("INSERT INTO images (image) VALUES (?)", (image,))
-    if url:
-        cursor.execute("INSERT INTO images (url) VALUES (?)", (url,))
+# Create an input field for URLs
+url = st.text_input("Enter URL")
+
+# Save the URL to the database if provided
+if url:
+    cursor.execute('INSERT INTO images (url) VALUES (?)', (url,))
     conn.commit()
 
-# Retrieve and display all stored images or URLs from the database
-cursor.execute("SELECT * FROM images")
+# Retrieve and display the uploaded images and URLs from the database
+cursor.execute('SELECT * FROM images')
 results = cursor.fetchall()
-for result in results:
-    image_id, image_blob, image_url = result
-    if image_blob:
-        st.image(image_blob, caption="Uploaded Image")
-    elif image_url:
-        try:
-            response = requests.get(image_url)
-            if response.status_code == 200:
-                pil_image = Image.open(BytesIO(response.content))
-                st.image(pil_image, caption="URL Image")
 
-                # Create a button to download the image
-                file_name = f"image_{image_id}.png"
-                st.download_button("Download Image", data=response.content, file_name=file_name)
-        except Exception as e:
-            st.error(f"Error loading image from URL: {e}")
+for result in results:
+    if result[1].startswith('http'):
+        st.markdown(f"URL: [{result[1]}]({result[1]})")
+    else:
+        st.image(result[1])
+
+# Create a button to download the images
+if st.button("Download Images"):
+    for result in results:
+        if not result[1].startswith('http'):
+            img_data = result[1]
+            st.download_button(f"Download Image {result[0]}", img_data, file_name=f"image{result[0]}")
 
 # Close the database connection
 cursor.close()
