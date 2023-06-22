fe4f3302c777adaf4caafc86a3d6ae564d081fd0
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-23 00:08:37 +0100

    Update ImageAndUrl.py

--- ImageAndUrl.py
+++ ImageAndUrl.py
@@ -1,18 +1,11 @@
 import streamlit as st
-import sqlite3
+import os
+import base64
 
-# Create a connection to the SQLite database
-conn = sqlite3.connect('image_urls.db')
-cursor = conn.cursor()
-
-# Create a table to store image URLs
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS images (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        url TEXT NOT NULL
-    )
-''')
-conn.commit()
+# Create a folder for storing uploaded images if it doesn't exist
+UPLOAD_FOLDER = "uploaded_images"
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 # Set Streamlit page configuration
 st.set_page_config(
@@ -22,44 +15,44 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-# Display the app title
+# Page title
 st.title('Bac 2024 doc')
 
-# Create a file uploader for images
-uploaded_files = st.file_uploader("Upload Images", accept_multiple_files=True, type=['png', 'jpg', 'jpeg'])
+# Upload image function
+def upload_image():
+    uploaded_files = st.file_uploader("Upload Images", accept_multiple_files=True, type=["png", "jpg", "jpeg"])
+    
+    if uploaded_files:
+        for uploaded_file in uploaded_files:
+            # Save the uploaded image to the upload folder
+            image_path = os.path.join(UPLOAD_FOLDER, uploaded_file.name)
+            with open(image_path, "wb") as f:
+                f.write(uploaded_file.getbuffer())
+            
+            # Display the uploaded image
+            st.image(image_path, caption=uploaded_file.name, use_column_width=True)
+            
+            # Create a download link for the uploaded image
+            download_link = get_download_link(image_path, uploaded_file.name)
+            st.markdown(download_link, unsafe_allow_html=True)
 
-# Loop over uploaded images and save them to the database
-for uploaded_file in uploaded_files:
-    if uploaded_file is not None:
-        file_bytes = uploaded_file.read()
-        cursor.execute('INSERT INTO images (url) VALUES (?)', (file_bytes,))
-        conn.commit()
+# Generate a download link for a file
+def get_download_link(file_path, file_name):
+    with open(file_path, "rb") as f:
+        file_data = f.read()
+    encoded_file = base64.b64encode(file_data).decode()
+    href = f'<a href="data:application/octet-stream;base64,{encoded_file}" download="{file_name}">Download {file_name}</a>'
+    return href
 
-# Create an input field for URLs
-url = st.text_input("Enter URL")
+# Show uploaded images from the upload folder
+uploaded_images = os.listdir(UPLOAD_FOLDER)
+if uploaded_images:
+    st.subheader("Uploaded Images")
+    for image_name in uploaded_images:
+        image_path = os.path.join(UPLOAD_FOLDER, image_name)
+        st.image(image_path, caption=image_name, use_column_width=True)
+        download_link = get_download_link(image_path, image_name)
+        st.markdown(download_link, unsafe_allow_html=True)
 
-# Save the URL to the database if provided
-if url:
-    cursor.execute('INSERT INTO images (url) VALUES (?)', (url,))
-    conn.commit()
-
-# Retrieve and display the uploaded images and URLs from the database
-cursor.execute('SELECT * FROM images')
-results = cursor.fetchall()
-
-for result in results:
-    if isinstance(result[1], bytes):
-        st.image(result[1])
-    else:
-        st.markdown(f"URL: [{result[1]}]({result[1]})")
-
-# Create a button to download the images
-if st.button("Download Images"):
-    for result in results:
-        if isinstance(result[1], bytes):
-            img_data = result[1]
-            st.download_button(f"Download Image {result[0]}", img_data, file_name=f"image{result[0]}.jpg")
-
-# Close the database connection
-cursor.close()
-conn.close()
+# Upload images
+upload_image()
