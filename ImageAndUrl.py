6aaa6856fab331acc58008a7886ebfb7ae71fd52
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-22 23:36:05 +0100

    Update ImageAndUrl.py

--- ImageAndUrl.py
+++ ImageAndUrl.py
@@ -1,1 +1,9 @@
 import streamlit as st
+
+st.set_page_config(
+    page_title="4eme secondaire image and url",
+    page_icon="",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
+st.title('Bac 2024 doc ')
