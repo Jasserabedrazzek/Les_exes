f575e0ec8fd3587a36e8d253506f44ec7031f374
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-22 21:26:49 +0100

    Update 4eme.py

--- 4eme.py
+++ 4eme.py
@@ -19,25 +19,14 @@ st.header("Upload Files PDF or Doc ")
 upload_folder = "uploads"
 os.makedirs(upload_folder, exist_ok=True)
 
-# Create a database connection
-conn = sqlite3.connect('file_uploads.db')
-c = conn.cursor()
-
-# Create a table to store file details
-c.execute('''CREATE TABLE IF NOT EXISTS files
-             (name TEXT, data BLOB)''')
-conn.commit()
-
 # Function to save uploaded file to the database with retry mechanism
-def save_file_to_db_with_retry(file):
+def save_file_to_db_with_retry(file_path, file_name, file_data):
     retries = 0
     while True:
         try:
-            with open(file, 'rb') as f:
-                file_data = f.read()
-            file_name = Path(file).name
-            c.execute("INSERT INTO files (name, data) VALUES (?, ?)", (file_name, sqlite3.Binary(file_data)))
-            conn.commit()
+            with sqlite3.connect('file_uploads.db') as conn:
+                c = conn.cursor()
+                c.execute("INSERT INTO files (name, data) VALUES (?, ?)", (file_name, sqlite3.Binary(file_data)))
             st.success("File uploaded successfully.")
             break
         except sqlite3.OperationalError as e:
@@ -52,11 +41,17 @@ def save_file_to_db_with_retry(file):
 
 # Function to retrieve file from the database
 def retrieve_file_from_db(file_name):
-    c.execute("SELECT data FROM files WHERE name=?", (file_name,))
-    file_data = c.fetchone()
-    if file_data is not None:
-        return file_data[0]
-    return None
+    with sqlite3.connect('file_uploads.db') as conn:
+        c = conn.cursor()
+        c.execute("SELECT data FROM files WHERE name=?", (file_name,))
+        file_data = c.fetchone()[0]
+    return file_data
+
+# Create a database connection and table if not exists
+with sqlite3.connect('file_uploads.db') as conn:
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS files
+                 (name TEXT, data BLOB)''')
 
 # Create an upload button
 file = st.file_uploader("Upload files (PDF, DOC)", type=["pdf", "doc"])
@@ -65,7 +60,7 @@ if file is not None:
     file_path = os.path.join(upload_folder, file.name)
     with open(file_path, "wb") as f:
         f.write(file.getbuffer())
-    save_file_to_db_with_retry(file_path)
+    save_file_to_db_with_retry(file_path, file.name, file.getbuffer())
 
 # Display uploaded files
 uploaded_files = [f for f in os.listdir(upload_folder) if os.path.isfile(os.path.join(upload_folder, f))]
@@ -73,14 +68,12 @@ if len(uploaded_files) > 0:
     st.header("Les séries")
     for uploaded_file in uploaded_files:
         file_data = retrieve_file_from_db(uploaded_file)
-        if file_data is not None:
-            st.write(uploaded_file)
-            download_button = st.button("Download", key=uploaded_file)
-            if download_button:
-                b64_data = base64.b64encode(file_data).decode()
-                href = f'<a href="data:application/octet-stream;base64,{b64_data}" download="{uploaded_file}">Click to download</a>'
-                st.markdown(href, unsafe_allow_html=True)
-        else:
-            st.warning(f"File '{uploaded_file}' is not available.")
+        st.write(uploaded_file)
+        download_button = st.button("Download", key=uploaded_file)
+        if download_button:
+            b64_data = base64.b64encode(file_data).decode()
+            href = f'<a href="data:application/octet-stream;base64,{b64_data}" download="{uploaded_file}">Click to download</a>'
+            st.markdown(href, unsafe_allow_html=True)
+
 st.write("---")
 st.markdown("Copyright © 2023 [Edu](#) . All Rights Reserved.")
