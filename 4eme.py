ac88de4cea3d355cbee0b15051bf8963fdcecedc
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-23 00:20:35 +0100

    Update 4eme.py

--- 4eme.py
+++ 4eme.py
@@ -1,18 +1,35 @@
 import streamlit as st
 import os
+import base64
 import sqlite3
 
 # Create a connection to the SQLite database
-conn = sqlite3.connect('documents.db')
+conn = sqlite3.connect('uploads.db')
 c = conn.cursor()
 
-# Create a table to store the uploaded documents
-c.execute('''
-    CREATE TABLE IF NOT EXISTS documents (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        filename TEXT
-    )
-''')
+# Create the 'uploads' table if it doesn't exist
+c.execute('''CREATE TABLE IF NOT EXISTS uploads
+             (file_name TEXT, file_data BLOB)''')
+
+# Function to save file to the database
+def save_file_to_db(file):
+    file_data = file.read()
+    file_name = file.name
+    c.execute("INSERT INTO uploads (file_name, file_data) VALUES (?, ?)", (file_name, file_data))
+    conn.commit()
+
+# Function to retrieve all uploaded files from the database
+def get_uploaded_files():
+    c.execute("SELECT file_name FROM uploads")
+    files = c.fetchall()
+    return files
+
+# Function to download a file from the database
+def download_file(file_name):
+    c.execute("SELECT file_data FROM uploads WHERE file_name=?", (file_name,))
+    file_data = c.fetchone()[0]
+    b64_data = base64.b64encode(file_data).decode()
+    return f'<a href="data:application/octet-stream;base64,{b64_data}" download="{file_name}">Download</a>'
 
 # Set Streamlit page configuration
 st.set_page_config(
@@ -21,47 +38,24 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-# Display the page title
+# Main title
 st.title('Bac 2024 doc')
 
-# Upload file function
-def upload_file():
-    uploaded_file = st.file_uploader("Upload PDF or DOC file", type=["pdf", "docx"], accept_multiple_files=True)
-    if uploaded_file is not None:
-        for file in uploaded_file:
-            filename = file.name
-            save_uploaded_file(file, filename)
-            # Save the filename to the database
-            c.execute("INSERT INTO documents (filename) VALUES (?)", (filename,))
-            conn.commit()
-        st.success("File(s) uploaded successfully!")
+# File upload section
+file = st.file_uploader("Upload PDF, DOC, or Image", type=["pdf", "doc", "jpeg", "jpg", "png"])
 
-# Save the uploaded file to a folder
-def save_uploaded_file(uploaded_file, filename):
-    with open(os.path.join("uploads", filename), "wb") as f:
-        f.write(uploaded_file.getbuffer())
+# Upload the file and save it to the database
+if file is not None:
+    save_file_to_db(file)
+    st.success('File uploaded successfully!')
 
-# Display the uploaded documents
-c.execute("SELECT * FROM documents")
-documents = c.fetchall()
-if documents:
-    st.subheader("Uploaded Documents:")
-    for doc in documents:
-        download_url = f"uploads/{doc[1]}"
-        st.markdown(f"- [{doc[1]}]({download_url})")
-
-# Download file function
-def download_file(filename):
-    st.markdown(f'<a href="uploads/{filename}" download="{filename}">Download {filename}</a>', unsafe_allow_html=True)
-
-# Display the upload file section
-st.subheader("Upload File(s):")
-upload_file()
-
-# Download file section
-st.subheader("Download File(s):")
-for doc in documents:
-    download_file(doc[1])
-
-# Close the database connection
-conn.close()
+# Show uploaded files
+uploaded_files = get_uploaded_files()
+if uploaded_files:
+    st.subheader('Uploaded Files')
+    for file in uploaded_files:
+        file_name = file[0]
+        st.write(file_name)
+        st.markdown(download_file(file_name), unsafe_allow_html=True)
+else:
+    st.subheader('No files uploaded yet.')
