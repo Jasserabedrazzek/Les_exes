f487b12ce9ee90fdf524d0cc0c2287c450dd0633
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-24 14:57:41 +0100

    Update 2eme.py

--- 2eme.py
+++ 2eme.py
@@ -1,81 +1,99 @@
 import streamlit as st
 import os
-from pathlib import Path
-import base64
+import shutil
+from PIL import Image
 import sqlite3
-import time
 
+# Create directories to store uploaded files
+PDF_DIRECTORY = "pdf"
+IMAGE_DIRECTORY = "image"
+if not os.path.exists(PDF_DIRECTORY):
+    os.makedirs(PDF_DIRECTORY)
+if not os.path.exists(IMAGE_DIRECTORY):
+    os.makedirs(IMAGE_DIRECTORY)
+
+# Set Streamlit page configuration
 st.set_page_config(
-    page_title="2eme secondaire",
-    page_icon="",
+    page_title="4eme secondaire Pdf",
+    page_icon=":blue_book:",
     layout="centered",
     initial_sidebar_state="expanded",
 )
 
-st.title("2eme secondaire")
-st.write("---")
-st.header("Upload Files PDF or Doc ")
-
-upload_folder = "uploads"
-os.makedirs(upload_folder, exist_ok=True)
-
 # Create a database connection
-conn = sqlite3.connect('file_uploads.db')
+conn = sqlite3.connect("urls.db")
 c = conn.cursor()
 
-# Create a table to store file details
-c.execute('''CREATE TABLE IF NOT EXISTS files
-             (name TEXT, data BLOB)''')
-conn.commit()
+# Create a table to store URLs if it doesn't exist
+c.execute('''CREATE TABLE IF NOT EXISTS urls (url text)''')
 
-# Function to save uploaded file to the database with retry mechanism
-def save_file_to_db_with_retry(file):
-    retries = 0
-    while True:
-        try:
-            with open(file, 'rb') as f:
-                file_data = f.read()
-            file_name = Path(file).name
-            c.execute("INSERT INTO files (name, data) VALUES (?, ?)", (file_name, sqlite3.Binary(file_data)))
-            conn.commit()
-            st.success("File uploaded successfully.")
-            break
-        except sqlite3.OperationalError as e:
-            if 'database is locked' in str(e).lower() and retries < 5:
-                retries += 1
-                delay = 2 ** retries  # exponential backoff
-                st.warning(f"Database is locked. Retrying in {delay} seconds...")
-                time.sleep(delay)
-            else:
-                st.error("Failed to upload file. Please try again later.")
-                break
+# Function to handle file uploads
+def handle_file_upload(file):
+    file_name = file.name
+    if file_name.endswith((".pdf", ".doc")):
+        file_path = os.path.join(PDF_DIRECTORY, file_name)
+        with open(file_path, "wb") as f:
+            f.write(file.getbuffer())
+        st.success(f"File uploaded: {file_name}")
+    elif file_name.endswith((".jpg", ".jpeg", ".png")):
+        file_path = os.path.join(IMAGE_DIRECTORY, file_name)
+        with open(file_path, "wb") as f:
+            f.write(file.getbuffer())
+        st.success(f"Image uploaded: {file_name}")
 
-# Function to retrieve file from the database
-def retrieve_file_from_db(file_name):
-    c.execute("SELECT data FROM files WHERE name=?", (file_name,))
-    file_data = c.fetchone()[0]
-    return file_data
+# Function to download a file
+def download_file(file_path, file_name):
+    with open(file_path, "rb") as f:
+        file_bytes = f.read()
+    st.download_button("Download", file_bytes, file_name)
 
-# Create an upload button
-file = st.file_uploader("Upload files (PDF, DOC)", type=["pdf", "doc"])
+# Function to save URL in the database
+def save_url(url):
+    c.execute("INSERT INTO urls (url) VALUES (?)", (url,))
+    conn.commit()
 
+# Display the title
+st.title('Bac 2024 doc')
+
+# Display the file upload section
+file = st.file_uploader("Upload PDF, DOC, or Image file", accept_multiple_files=True)
 if file is not None:
-    file_path = os.path.join(upload_folder, file.name)
-    with open(file_path, "wb") as f:
-        f.write(file.getbuffer())
-    save_file_to_db_with_retry(file_path)
+    for uploaded_file in file:
+        handle_file_upload(uploaded_file)
 
-# Display uploaded files
-uploaded_files = [f for f in os.listdir(upload_folder) if os.path.isfile(os.path.join(upload_folder, f))]
-if len(uploaded_files) > 0:
-    st.header("Les séries")
-    for uploaded_file in uploaded_files:
-        file_data = retrieve_file_from_db(uploaded_file)
-        st.write(uploaded_file)
-        download_button = st.button("Download", key=uploaded_file)
-        if download_button:
-            b64_data = base64.b64encode(file_data).decode()
-            href = f'<a href="data:application/octet-stream;base64,{b64_data}" download="{uploaded_file}">Click to download</a>'
-            st.markdown(href, unsafe_allow_html=True)
+# Display the URL input and save button
+url = st.text_input("Enter URL")
+save_button = st.button("Save URL")
+if save_button and url:
+    save_url(url)
+    st.success("URL saved!")
+
+# Display the uploaded files
+uploaded_files = os.listdir(PDF_DIRECTORY) + os.listdir(IMAGE_DIRECTORY)
+st.subheader("Uploaded Files")
+for file_name in uploaded_files:
+    st.write(file_name)
+    if file_name.endswith((".pdf", ".doc")):
+        file_path = os.path.join(PDF_DIRECTORY, file_name)
+        download_file(file_path, file_name)
+    elif file_name.endswith((".jpg", ".jpeg", ".png")):
+        file_path = os.path.join(IMAGE_DIRECTORY, file_name)
+        image = Image.open(file_path)
+        st.image(image)
+        download_file(file_path, file_name)
+
+# Display the saved URLs
+st.subheader("Saved URLs")
+c.execute("SELECT * FROM urls")
+urls = c.fetchall()
+for url in urls:
+    st.write(url[0])
+
+# Close the database connection
+conn.close()
+
 st.write("---")
 st.markdown("Copyright © 2023 [Edu](#) . All Rights Reserved.")
+st.write("Rabi ynajhna kol, amin ya rabi :open_hands:")
+st.write("Jasser ")
+st.write("Bac 2024 admis") 
